d14865b5eb4a934c6aa98cd8f1fc5aab65b166f2
Darius G <dariusphp@gmail.com>
2015-06-16 16:20:03 +0300

    added second test

--- test/Nfq/EventManagerAddTest.php
+++ test/Nfq/EventManagerAddTest.php
@@ -3,7 +3,7 @@ namespace Nfq;
 
 class EventManagerAddTest extends \PHPUnit_Framework_TestCase
 {
-    /** @var  EventManager $eventManager */
+    /** @var EventManager $eventManager */
     private $eventManager;
 
     public function setUp()
@@ -19,5 +19,12 @@ class EventManagerAddTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($name, $event->name);
         $this->assertInstanceOf('\\DateTime', $event->date);
+    }
+
+    public function testAddMultipleEvents() {
+        $this->eventManager->addEvent("First event", new \DateTime(), 1);
+        $this->eventManager->addEvent("Second event", new \DateTime(), 2);
+        
+        $this->assertEquals(2, $this->eventManager->getEventList()->count());
     }
 }
