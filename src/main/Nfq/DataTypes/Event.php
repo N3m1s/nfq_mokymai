fb0517eb430f9db8efce5e018167daf4e6c5afcd
Paulius K <pauliuskm@gmail.com>
2015-06-16 15:37:14 +0300

    added Event and EventList base data types

---
+++ src/main/Nfq/DataTypes/Event.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LauraK
+ * Date: 2015-06-16
+ * Time: 15:36
+ */
+
+namespace Nfq\DataTypes;
+
+/**
+ * Class Event
+ * @package Nfq\DataTypes
+ */
+class Event
+{
+
+}
