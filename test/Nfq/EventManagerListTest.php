005b5a7ed842c9a44b3ab6d874eb555d110dacb4
Paulius K <pauliuskm@gmail.com>
2015-06-16 16:09:30 +0300

    added date parameter

--- test/Nfq/EventManagerListTest.php
+++ test/Nfq/EventManagerListTest.php
@@ -14,11 +14,24 @@ namespace Nfq;
  */
 class EventManagerListTest extends \PHPUnit_Framework_TestCase
 {
+    private $eventManager;
+
+    public function setUp()
+    {
+        $this->eventManager = new EventManager();
+    }
+
     public function testIfListOfEventsIsReturned()
     {
-        $eventManager = new EventManager();
+        $eventList = $this->eventManager->getEventList();
 
-        $eventList = $eventManager->getEventList();
+        $this->assertInstanceOf('\\Nfq\\DataTypes\\EventList', $eventList);
+    }
+
+    public function testIfListOfEventsForGivenDateIsReturned()
+    {
+        $today = new \DateTime();
+        $eventList = $this->eventManager->getEventList($today);
 
         $this->assertInstanceOf('\\Nfq\\DataTypes\\EventList', $eventList);
     }
